af86ea589171ba24d942181e263a16b2eb201272
paulbg <paulbg@hey.com>
2024-09-07 14:58:28 +0300

    feat: implement creating bluesky posts

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -2,20 +2,13 @@
 
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import bskyAgent from "@/api/bskyAgent";
+import { post } from "@/api/bluesky/post";
 
 export default function Post() {
   const [content, setContent] = useState("");
 
   const handleClick = async () => {
-    await bskyAgent.login({
-      identifier: "",
-      password: "",
-    });
-
-    await bskyAgent.post({
-      text: content,
-    });
+    await post(content);
   };
 
   return (
@@ -25,7 +18,7 @@ export default function Post() {
         <textarea
           onChange={(e) => setContent(e.target.value)}
           value={content}
-          className="w-full border-2 border-gray-100 p-3 rounded-xl resize-none h-full rounded-md focus:outline-none"
+          className="w-full border-2 border-gray-100 p-3 rounded-xl resize-none h-full focus:outline-none"
           placeholder="What's up?"
         />
         <div className="flex justify-between items-center mt-2">
