422355bc687f80d00aaf458485060311d9201e9f
paulbg <paulbg@hey.com>
2024-09-13 15:54:47 +0300

    feat: implement send-scheduled-posts api

--- src/app/api/send-scheduled-posts/route.ts
+++ src/app/api/send-scheduled-posts/route.ts
@@ -1,5 +1,37 @@
+import { getScheduledSkeetByUserHandle } from "@/app/actions/skeets/scheduledSkeets";
+import { getSession } from "@/lib/auth/session";
+import { createAgent } from "@/lib/bsky/agent";
+
 export const dynamic = "force-dynamic";
 
-export function GET(request: Request) {
-  return new Response("Not implemented");
+export async function GET(req: Request, res: Response) {
+  const session = await getSession();
+
+  const bskySession = session?.user?.email;
+  const skeets = await getScheduledSkeetByUserHandle(bskySession?.handle);
+
+  if (skeets.length === 0) {
+    return new Response("No skeets to post");
+  }
+
+  const now = new Date();
+
+  const shouldBePosted = skeets.filter((skeet) => {
+    return skeet.postAt!.getTime() <= now.getTime();
+  });
+
+  if (shouldBePosted.length === 0) {
+    return new Response("No skeets to post");
+  }
+
+  const agent = createAgent();
+  agent.sessionManager.session = session?.user?.email;
+
+  for (const skeet of shouldBePosted) {
+    await agent.post({
+      text: skeet.content!,
+    });
+  }
+
+  return new Response("Posted skeets");
 }
