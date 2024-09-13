b801aa088d033949221e4beabb1c66173e3cab36
paulbg <paulbg@hey.com>
2024-09-13 12:41:33 +0300

    feat: implement simple sidebar for dashboard

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import Sidebar from "@/components/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="flex h-screen">
+          <Sidebar />
+          <main className="flex-1 overflow-auto p-4">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
