d2da291a2dfc838962e97b1c6865ee940cfb8dbf
paulbg <paulbg@hey.com>
2024-09-13 09:50:04 +0300

    chore: agent -> createAgent

--- src/lib/bsky/agent.ts
+++ src/lib/bsky/agent.ts
@@ -1,5 +1,7 @@
 import { AtpAgent } from "@atproto/api"; // would be better to swap to Agent as the AtpAgent will be deprecated
 
-export const agent = new AtpAgent({
-  service: "https://bsky.social",
-});
+export const createAgent = () => {
+  return new AtpAgent({
+    service: "https://bsky.social",
+  });
+};
