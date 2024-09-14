425bc91317c9ad60f46d152db2c31ebbe7ccbc04
paulbg <paulbg@hey.com>
2024-09-14 10:24:26 +0300

    feat: implement react-query in sidebar

--- src/features/sidebar.tsx
+++ src/features/sidebar.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import { Button } from "../components/ui/button";
 import {
   createDraft,
@@ -10,67 +9,62 @@ import { useSession } from "next-auth/react";
 import LoadingSpinner from "@/components/loading-spinner";
 import { Draft } from "@/components/draft";
 import { useRouter } from "next/navigation";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { queryClient } from "../lib/react-query/client";
 
 export default function Sidebar() {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
-  const [drafts, setDrafts] = useState<any[]>([]);
-
   const { data: session } = useSession();
 
-  useEffect(() => {
+  const fetchDrafts = async () => {
     const handle = session?.user?.email?.handle;
-
-    const getDrafts = async () => {
-      setIsLoading(true); // Start loading
-      try {
-        if (handle) {
-          const drafts = await getDraftsByUserHandle(handle);
-          setDrafts(drafts);
-        }
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setIsLoading(false); // Stop loading after fetching
-      }
-    };
-
-    getDrafts();
-  }, [session]); // Removed drafts from dependency array
-
-  const handleCreateDraft = async () => {
-    const handle = session?.user?.email?.handle;
-
-    if (!handle) {
-      return;
-    }
-
-    // todo: go to blank /dashboard/post
-    const [draft] = await createDraft({
-      userHandle: handle,
-      content: "",
-    });
-    router.push(`/dashboard/post/${draft.id}`);
+    return await getDraftsByUserHandle(handle);
   };
 
-  // todo: subscribe to the drafts table so that the sidebar updates when a draft is created
+  const { data: drafts = [], isPending } = useQuery({
+    queryKey: ["drafts"],
+    queryFn: fetchDrafts,
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: createDraft,
+    onSuccess: (data) => {
+      const [draft] = data;
+
+      queryClient.invalidateQueries({ queryKey: ["drafts"] });
+      router.push(`/dashboard/post/${draft.id}`);
+    },
+  });
+
+  const handleAddDraft = async () => {
+    const handle = session?.user?.email?.handle;
+    console.log({ handle });
+
+    const content = "";
+
+    await mutate({ content, userHandle: handle });
+  };
 
   return (
     <aside className="w-64 min-h-screen border-r-[1px] text-gray-800">
       <nav className="p-4 flex flex-col">
-        <Button onClick={handleCreateDraft} variant="ghost">
+        <Button onClick={() => handleAddDraft()} variant="ghost">
           New Draft
         </Button>
         <div className="border-t-[1px]">
           <div className="mt-4">
-            {isLoading ? (
+            {isPending ? (
               <LoadingSpinner size="sm" />
-            ) : (
+            ) : drafts.length > 0 ? (
               <ul className="space-y-2">
                 {drafts.map((draft) => (
                   <Draft key={draft.id} id={draft.id} content={draft.content} />
                 ))}
               </ul>
+            ) : (
+              <p className="text-center text-sm italic text-gray-500">
+                No drafts found
+              </p>
             )}
           </div>
         </div>
