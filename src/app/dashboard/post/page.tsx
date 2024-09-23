334f03493b88f81e27052a918f39f17b54f63289
paulbg <paulbg@hey.com>
2024-09-23 17:39:51 +0300

    chore: remove dynamic routing in new post

---
+++ src/app/dashboard/post/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { NewPost } from "@/features/new-post";
+
+export default function Post() {
+  return (
+    <main className="flex my-[28vh] justify-center">
+      <NewPost />
+    </main>
+  );
+}
