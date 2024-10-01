9ce4b19665fa3ed3d63773a85a68e51309b09582
paulbg <paulbg@hey.com>
2024-10-01 17:59:54 +0300

    chore: draft update
    - highlight the draft it it's the one being edited
    - move draft to /features as it now has a complex logic

--- src/features/draft.tsx
+++ src/features/draft.tsx
@@ -5,16 +5,21 @@ import { useMutation } from "@tanstack/react-query";
 import { queryClient } from "@/lib/react-query/client";
 
 export const Draft = ({ id, content }: { id: string; content: string }) => {
-  const { setCurrentDraftId } = useCurrentDraftContext();
+  const { currentDraftId, setCurrentDraftId } = useCurrentDraftContext();
 
   const { mutate: deleteDraft } = useMutation({
     mutationFn: deleteDrafts,
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ["drafts"] }),
   });
 
+  const draftShadow =
+    currentDraftId === id ? "shadow-blue-400" : "shadow-gray-300";
+
   return (
     <div onClick={() => setCurrentDraftId(id)} className="cursor-pointer">
-      <div className="flex flex-col pt-2 pl-3 text-sm duration-200 shadow-md hover:bg-gray-100 h-36 rounded-xl">
+      <div
+        className={`flex flex-col pt-2 pl-3 text-sm duration-200 shadow-md h-36 rounded-xl ${draftShadow}`}
+      >
         <button
           onClick={() => deleteDraft(id)}
           className="flex items-center self-end justify-center w-6 h-6 mr-3 text-gray-500 duration-200 bg-gray-100 rounded-full hover:bg-gray-300"
