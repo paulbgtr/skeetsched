dc5c1a949d0b9ab29c6221d24bef152a433af825
paulbg <paulbg@hey.com>
2024-10-03 13:04:23 +0300

    chore: remove maxLength prop from editor

--- src/features/editor/components/post-text-area.tsx
+++ src/features/editor/components/post-text-area.tsx
@@ -4,16 +4,11 @@ interface PostTextAreaProps {
   maxLength: number;
 }
 
-export const PostTextArea = ({
-  value,
-  onChange,
-  maxLength,
-}: PostTextAreaProps) => (
+export const PostTextArea = ({ value, onChange }: PostTextAreaProps) => (
   <textarea
     onChange={onChange}
     value={value}
     className="w-full h-full p-3 border-[1px] border-gray-700 resize-none rounded-xl focus:outline-none bg-transparent"
     placeholder="What's up?"
-    maxLength={maxLength}
   />
 );
