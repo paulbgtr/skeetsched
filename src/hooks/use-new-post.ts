9811d00bd368bf66c738b37c2b3976f8fcc92997
paulbg <paulbg@hey.com>
2024-10-03 11:32:43 +0300

    feat: implement use-new-post hook

---
+++ src/hooks/use-new-post.ts
@@ -0,0 +1,115 @@
+import { useState, useMemo, useEffect } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import debounce from "lodash.debounce";
+import useAgent from "@/hooks/bsky/use-agent";
+import { useToast } from "@/hooks/shadcn/use-toast";
+import { useCurrentDraftContext } from "@/context/current-draft-context";
+import { queryClient } from "@/lib/react-query/client";
+import { createScheduledSkeet } from "@/app/actions/posts/scheduled-posts";
+import {
+  deleteDrafts,
+  updateDrafts,
+  getDraftById,
+} from "@/app/actions/posts/drafts";
+import { formatDateForNotification } from "@/lib/utils";
+
+const useNewPost = () => {
+  const { toast } = useToast();
+  const { agent } = useAgent();
+  const { currentDraftId } = useCurrentDraftContext();
+  const [content, setContent] = useState("");
+
+  const { data: draft } = useQuery({
+    queryKey: ["draft", currentDraftId],
+    queryFn: () => getDraftById(currentDraftId), // todo: fix type err
+    enabled: !!currentDraftId,
+  });
+
+  useEffect(() => {
+    if (draft) setContent(draft[0]?.content || "");
+  }, [currentDraftId, draft]);
+
+  const cleanUp = (key?: string) => {
+    key && queryClient.invalidateQueries({ queryKey: [key] });
+    if (currentDraftId) {
+      deleteDraft(currentDraftId);
+    }
+    setContent("");
+  };
+
+  const { mutate: updateDraft } = useMutation({
+    mutationFn: async (content: string) => {
+      if (!currentDraftId) return;
+      await updateDrafts(currentDraftId, { content });
+    },
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["drafts"] }),
+  });
+
+  const { mutate: deleteDraft } = useMutation({
+    mutationFn: deleteDrafts,
+    onSuccess: () => cleanUp("drafts"),
+  });
+
+  const { mutate: schedulePost, isPending: isPendingSchedulePost } =
+    useMutation({
+      mutationFn: createScheduledSkeet,
+      onSuccess: ([data]) => {
+        const { postAt } = data;
+        if (!postAt) return;
+
+        toast({
+          title: "Skeet scheduled",
+          description: formatDateForNotification(postAt),
+        });
+        cleanUp("scheduled-posts");
+      },
+    });
+
+  const { mutate: addPost, isPending: isPendingAddPost } = useMutation({
+    mutationFn: async () => {
+      await agent?.post({ text: content });
+    },
+    onSuccess: () => {
+      toast({
+        title: "Skeet posted",
+        description: "Your skeet has been posted",
+      });
+      cleanUp();
+    },
+  });
+
+  const debouncedSaveDraft = useMemo(
+    () => debounce((value: string) => updateDraft(value), 500),
+    [updateDraft]
+  );
+
+  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+    debouncedSaveDraft(e.target.value);
+  };
+
+  const handleSchedulePost = async (postAt: Date) => {
+    if (!agent?.sessionManager?.session) return;
+
+    try {
+      schedulePost({
+        userHandle: agent.sessionManager.session.handle,
+        content,
+        postAt,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return {
+    content,
+    handleContentChange,
+    handleSchedulePost,
+    handlePost: () => addPost(),
+    isPendingSchedulePost,
+    isPendingAddPost,
+  };
+};
+
+export default useNewPost;
