c3a6e6700f0cfedb7d44bd05d608d3b14f6bdf2a
paulbg <paulbg@hey.com>
2024-10-05 09:40:20 +0300

    chore: use use-drafts hook in drafts-tab

--- src/features/sidebar/drafts/drafts-tab.tsx
+++ src/features/sidebar/drafts/drafts-tab.tsx
@@ -1,15 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { useState, useEffect } from "react";
-import { useQuery, useMutation } from "@tanstack/react-query";
-import {
-  getDraftsByHandle,
-  createDraft,
-} from "../../../app/actions/posts/drafts";
-import { useSession } from "next-auth/react";
-import { useCurrentDraftContext } from "@/context/current-draft-context";
-import { queryClient } from "@/lib/react-query/client";
+import useDrafts from "@/hooks/use-drafts";
 
 import LoadingSpinner from "@/components/loading-spinner";
 import { Draft } from "@/features/sidebar/drafts/draft";
@@ -17,46 +9,13 @@ import { Button } from "../../../components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 export const DraftsTab = () => {
-  const { data: session } = useSession();
-  const { setCurrentDraftId } = useCurrentDraftContext();
-
-  const [handle, setHandle] = useState<string>("");
-
-  useEffect(() => {
-    if (!session) {
-      return;
-    }
-    setHandle(session.user.handle);
-  }, [session]);
-
-  const { data: drafts, isPending } = useQuery({
-    queryKey: ["drafts"],
-    queryFn: async () => {
-      const drafts = await getDraftsByHandle(handle);
-      return drafts.sort().reverse();
-    },
-    enabled: !!handle,
-  });
-
-  const { mutate: createNewDraft } = useMutation({
-    mutationFn: createDraft,
-    onSuccess: (data) => {
-      queryClient.invalidateQueries({ queryKey: ["drafts"] });
-      setCurrentDraftId(data[0].id);
-    },
-  });
-
-  const handleCreateDraft = () => {
-    if (handle) {
-      createNewDraft({ handle, content: "" });
-    }
-  };
+  const { drafts, isPending, handleCreateDraft } = useDrafts();
 
   return (
     <>
       <Button
         className="w-full"
-        onClick={handleCreateDraft}
+        onClick={() => handleCreateDraft()}
         variant="secondary"
       >
         New Draft
