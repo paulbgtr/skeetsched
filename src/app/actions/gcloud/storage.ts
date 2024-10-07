68f17ef96b0e299906a3b8a415127da3eecdb2a1
paulbg <paulbg@hey.com>
2024-10-07 17:16:00 +0300

    feat: impl server actions for gcloud storage
    - generate signed url for gcloud storage
    - upload image to gcloud storage

---
+++ src/app/actions/gcloud/storage.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import { GetSignedUrlConfig, Storage } from "@google-cloud/storage";
+
+const storage = new Storage({
+  projectId: "skeetsched-436907",
+});
+
+const generateSignedUrl = async (fileName: string) => {
+  if (!process.env.BUCKET_NAME) {
+    throw new Error("BUCKET_NAME is not set");
+  }
+
+  const options = {
+    version: "v2",
+    action: "read",
+    expires: Date.now() + 1000 * 60 * 60, // one hour
+  } as GetSignedUrlConfig;
+
+  const [url] = await storage
+    .bucket(process.env.BUCKET_NAME)
+    .file(fileName)
+    .getSignedUrl(options);
+
+  return url;
+};
+
+export const uploadFile = async (
+  base64Data: string,
+  destination: string,
+  generationMatchPrecondition: number = 0
+) => {
+  if (!process.env.BUCKET_NAME) {
+    throw new Error("BUCKET_NAME is not set");
+  }
+
+  const options = {
+    destination: destination,
+    preconditionOpts: { ifGenerationMatch: generationMatchPrecondition },
+  };
+
+  try {
+    const base64Content = base64Data.replace(/^data:image\/\w+;base64,/, "");
+    const buffer = Buffer.from(base64Content, "base64");
+
+    await storage
+      .bucket(process.env.BUCKET_NAME)
+      .file(destination)
+      .save(buffer, options);
+
+    const url = await generateSignedUrl(destination);
+
+    console.log(`File uploaded to ${destination}`);
+
+    return url;
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw error;
+  }
+};
